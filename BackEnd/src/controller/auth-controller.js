c346d10b251dbfc5f3db24901aa3af5cb5056b5f
Mhbib34 <mhabib34official@gmail.com>
2025-03-19 15:22:08 +0700

    Add Login User Api

--- BackEnd/src/controller/auth-controller.js
+++ BackEnd/src/controller/auth-controller.js
@@ -42,3 +42,43 @@ export const register = async (req, res, next) => {
     next(error);
   }
 };
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new ResponseError(400, "Email and password are required");
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      throw new ResponseError(404, "User is not found");
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      throw new ResponseError(401, "Incorrect password");
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json({
+      success: true,
+      message: "User login successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
